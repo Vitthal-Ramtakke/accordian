54fd8dc2685eba7d9c8d91abce3192ba789d56a8
Vitthal-Ramtakke <vitthalramtakke3013@gmail.com>
2024-03-14 13:42:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
+
 
 function App() {
   return (
     <div className="App">
-      <RandomColor/>
+      <StarRating noOfStars={6}/>
     </div>
   );
 }
